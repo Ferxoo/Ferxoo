811a435a6c5c90ce04eea670cdf545616e346ee8
Ferxoo <130993165+Ferxoo@users.noreply.github.com>
2026-07-07 16:42:56 +0200

    Update scenario_central.m

--- irb1200_simulation/environment/scenario_central.m
+++ irb1200_simulation/environment/scenario_central.m
@@ -3,7 +3,7 @@ function obstacles = scenario_central()
 obs.name     = 'central_box';
 obs.geometry = collisionBox(0.180, 0.350, 0.450);
 obs.T_world  = trvec2tform([0.320, 0.000, 0.300]);
-obs.color    = [0.80, 0.20, 0.20];   % red
+obs.color    = [0.80, 0.20, 0.20];
 
 obs.geometry.Pose = obs.T_world;
 
